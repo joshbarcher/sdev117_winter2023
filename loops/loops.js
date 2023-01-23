ba3745c6f8c57b2edbd755b9251491d5fa084509
Josh Archer <jarcher@greenriver.edu>
2023-01-23 10:53:02 -0800

    Added an example of definite (for) loops.

---
+++ loops/loops.js
@@ -0,0 +1,13 @@
+
+//a good loop - print out the centennials from 1000 to 2100
+for (let year = 1000; year <= 2100; year += 100)
+{
+    console.log("Year", year);
+}
+
+//a bad loop - trying to print out "hello" 100 times
+for (let i = 1; i <= 100; i--)
+{
+    console.log("hello");
+}
+
