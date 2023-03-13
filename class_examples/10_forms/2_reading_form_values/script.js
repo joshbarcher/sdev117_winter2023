a98f0692d46ec4f654346b480c536d2767a77d46
Josh Archer <jarcher@greenriver.edu>
2023-03-13 10:31:46 -0700

    Added an example of manually validating a form.

--- class_examples/10_forms/2_reading_form_values/script.js
+++ class_examples/10_forms/2_reading_form_values/script.js
@@ -5,6 +5,35 @@ window.onload = function() {
 
 function summarize(event)
 {
+    //stop the default behavior (the form submitting)
+    event.preventDefault();
 
+    //read in our form values
+    let movieTitle = document.querySelector("#title").value;
+    let movieLength = document.querySelector("#length").value;
+
+    console.log(movieTitle);
+    console.log("Movie length is " + movieLength);
+
+    //loop over all genre elements and figure out which one is selected
+    let genre = "";
+    let radioButtons = document.querySelectorAll("input[name='genre']");
+    for (let i = 0; i < radioButtons.length; i++)
+    {
+        let button = radioButtons[i];
+        if (button.checked === true)
+        {
+            console.log("Genre is " + button.value);
+        }
+    }
+
+    let checkBoxes = document.querySelectorAll("input[type='checkbox']");
+    for (let i = 0; i < checkBoxes.length; i++)
+    {
+        if (checkBoxes[i].checked === true)
+        {
+            console.log(checkBoxes[i].value);
+        }
+    }
 }
 
--- class_examples/10_forms/3_form_validation/example/script.js
+++ class_examples/10_forms/3_form_validation/example/script.js
@@ -8,6 +8,42 @@ function validateForm(event)
     //prevent the form from submitting
     event.preventDefault();
 
+    //count the number of errors...
+    let errorCount = 0;
+
     //validate the form...
+    let name = document.querySelector("#name").value;
+    let error = document.querySelector("#name-error");
+
+    //make sure the name is between 3 and 10 characters
+    if (name.length < 3 || name.length > 10)
+    {
+        //show the error message
+        error.style.display = "inline";
+        errorCount++;
+    }
+    else
+    {
+        error.style.display = "none";
+    }
+
+    let phone = document.querySelector("#phone").value;
+    let phoneError = document.querySelector("#phone-error");
+
+    if (phone.length === 0)
+    {
+        phoneError.style.display = "inline";
+        errorCount++;
+    }
+    else
+    {
+        phoneError.style.display = "none";
+    }
+
+    //submit the form if there are no errors!
+    if (errorCount === 0)
+    {
+        document.forms["questionnaire"].submit();
+    }
 }
 
